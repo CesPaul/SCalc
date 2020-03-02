423154f46706034cf88c096050f23de7a09fa518
CesPaul PC <scrin333@gmail.com>
2020-03-02 18:13:45 +0700

    Настроил обработку нажатия кнопки расчёта.

--- app/src/main/java/com/cespaul/scalc/MainActivity.kt
+++ app/src/main/java/com/cespaul/scalc/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.cespaul.scalc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        calculate.setOnClickListener {
+            val s: String = expression.text.toString()
+            val n = ExpressionParser()
+            lateinit var expression: List<String>
+            try {
+                expression = n.parse(s)
+                val calculator = Calculator()
+                result.text = calculator.calc(expression).toString()
+            } catch (e: Exception) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
